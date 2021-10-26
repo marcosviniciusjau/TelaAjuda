495f6301e2275fc1ed6aa4369dacebfd0b4de7f1
Usuario <Usuario@CPD02-M07>
2021-10-26 14:31:41 -0300

    Adicionar arquivos de projeto.

---
+++ TelaAjuda/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TelaAjuda
+{
+  
+   
+
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            logo.Source = ImageSource.FromResource("TelaAjuda.Img.peixelogotipo.png");
+        }
+    }
+}
